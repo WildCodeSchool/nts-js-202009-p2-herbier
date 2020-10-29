711b177762f7db3cdb2e007e0fae9778612c27ed
Benoit2109 <benoitlebourdonnec@gmail.com>
2020-10-29 14:39:46 +0100

    frame page scanning plant ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components'
+import ScanPage from './components/ScanPage.jsx'
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <ScanPage />
+      
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Reader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ScanReader = styled.div`
+  background-color: #ABB2B9;
+  height: 150px;
+  width:100%;
+  ;
+`;
+
+const Reader = () => <ScanReader></ScanReader>;
+
+export default Reader;
