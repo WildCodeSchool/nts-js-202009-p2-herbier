d0737a839a110fd2a69a6f4c5b97b67409de57e2
Gwenvael.L <laskar.gwenvael@gmail.com>
2020-11-04 12:44:13 +0100

    filters structure almost finished

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -19,7 +19,9 @@ const ButtonAllSome = styled.div`
   justify-content: space-between;
   align-items: center;
   color: white;
-  background-color: rgba(${({all}) => all? '156, 214, 155': '226, 122, 112'});
+  background-color: rgba(
+    ${({ all }) => (all ? '156, 214, 155' : '226, 122, 112')}
+  );
   border-radius: 30px;
   box-shadow: rgb(0, 0, 0, 0.5) inset 0px 0px 10px 1px;
 `;
@@ -28,18 +30,17 @@ const WhiteCircle = styled.div`
   width: 30px;
   height: 30px;
   border-radius: 50%;
-  background-color:white;
-  transform: translate(${({all}) => all ? '-130px' : '0px' });
+  background-color: white;
+  transform: translate(${({ all }) => (all ? '-130px' : '0px')});
   box-shadow: rgb(0, 0, 0, 0.5) 0px 0px 4px 2px;
   transition: all 0.3s ease-in-out;
-
 `;
 
 const TextInButton = styled.p`
-  padding-left: ${({all})=> all?"40px":"10px"};
+  padding-left: ${({ all }) => (all ? '40px' : '10px')};
 `;
 
-const FilterAdvenced =styled.button`
+const FilterAdvenced = styled.button`
   background-color: white;
   color: rgba(226, 122, 112);
   border: 3px solid rgba(226, 122, 112);
@@ -49,77 +50,107 @@ const FilterAdvenced =styled.button`
   font-size: inherit;
 `;
 
-const ContainerFiltre =styled.div`
+const ContainerFiltre = styled.div`
   margin-top: 15px;
+  width:90%;
   display: flex;
   justify-content: space-around;
 `;
 
-const Title=styled.h3`
+const Title = styled.h3`
   margin-left: 1.3em;
 `;
 
-const Title4=styled.h4`
-
+const UlListe = styled.div`
+  padding-top:${({ filterChoice }) => (filterChoice ? '1.5rem' : '0')};
+  width: 90%;
+  display: ${({ filterChoice, filter }) =>
+    filter &&
+    (filterChoice === 'famille' ||
+      filterChoice === 'espece' ||
+      filterChoice === 'genre' ||
+      filterChoice === 'nom_de_site')
+      ? 'none'
+      : 'block'};
 `;
 
 const ButtonParamete = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  height:100%;
-  width:80px;
-  color:white;
-  background-color:rgba(156, 214, 155);
-  border-top-left-radius:6px;
-  border-top-right-radius:6px;
-  margin-right:8px;
-  margin-left:8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 60px;
+  color: ${({filterchoice})=>!filterchoice? 'white':'black'};
+  background-color: rgba(${({filterchoice,valueButton})=> filterchoice === valueButton? '79, 127, 99' :'156, 214, 155' });
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
 `;
 
-const ParameterFiltre=styled.div`
-  display:${({filter}) =>filter?"none":"flex"};
-  justify-content: center;
-  align-items:center;
-  height:5vh;
-  margin-top:20px;
-  `;
+const ParameterFiltre = styled.div`
+  display: ${({ filter }) => (filter ? 'none' : 'flex')};
+  justify-content: space-between;
+  width:90%;
+  margin:0 1.5rem 0 0;
+  align-items: center;
+  height: 5vh;
+  margin-top: 20px;
+`;
 
 const ListeFiltre = styled.div`
-  background-color: rgba(156, 214, 155);
-  height:fit-content;
-  width:90%;
-
+ color: ${({filterchoice})=>!filterchoice? 'white':'black'};
+  background-color: rgba(${({filterchoice})=>!filterchoice? '79, 127, 99' :'156, 214, 155' });
+  height: fit-content;
+  display: ${({ filter }) => (filter ? 'none' : 'flex')};
+  flex-direction: column;
+  width: 90%;
 `;
 
 const Li = styled.li`
-  list-style:none;
-  margin:0;
-  padding:0;
-  width:90%;
+  list-style: none;
+  padding: 4px 2px 4px 2rem;
+  width: 90%;
+  margin:0 1.5rem 0 0;
+
 `;
 
+const WindowFilter = styled.div`
+  display: ${({ filter }) => (filter ? 'none' : 'flex')};
+  width:90%;
+  margin:1.5rem;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+`;
 
 class Library extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { all: false, filter: false ,vegetals: [] ,choice: 'famille', tri:[], list:[]};
+    this.state = {
+      all: false,
+      filter: true,
+      vegetals: [],
+      choice: null,
+      tri: [],
+      list: [],
+    };
     this.getData = this.getData.bind(this);
   }
 
-  componentDidMount(){
-    this.getData()
+  componentDidMount() {
+    this.getData();
   }
 
-  getData(){
-  axios.get('https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_collection-vegetale-nantes&q=&rows=3923&facet=famille&facet=genre&facet=nom_du_site&facet=espece')
-    .then((res) => {
-      return this.setState({vegetals : res.data.records, tri:res.data.facet_groups})
-  })
-  }
-
-  getFamilly(event){
-    this.setState({ choice: event.target.value });
+  getData() {
+    axios
+      .get(
+        'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_collection-vegetale-nantes&q=&rows=3923&facet=famille&facet=genre&facet=nom_du_site&facet=espece'
+      )
+      .then((res) => {
+        return this.setState({
+          vegetals: res.data.records,
+          tri: res.data.facet_groups,
+        });
+      });
   }
 
   render() {
@@ -127,12 +158,15 @@ class Library extends React.Component {
       <div className="Library">
         <SearchBar />
         <ContainerFiltre>
-          <ButtonAllSome all={this.state.all}
-            onClick={()=>{
-              this.setState({all: !this.state.all });
+          <ButtonAllSome
+            all={this.state.all}
+            onClick={() => {
+              this.setState({ all: !this.state.all });
             }}
           >
-            <TextInButton all={this.state.all} >{this.state.all ? 'Toutes' : 'Ma collection'}</TextInButton>
+            <TextInButton all={this.state.all}>
+              {this.state.all ? 'Toutes' : 'Ma collection'}
+            </TextInButton>
             <WhiteCircle all={this.state.all} />
           </ButtonAllSome>
           <FilterAdvenced
@@ -144,37 +178,92 @@ class Library extends React.Component {
           >
             Avancées
           </FilterAdvenced>
-
         </ContainerFiltre>
+        <WindowFilter filter={this.state.filter}>
         <ParameterFiltre filter={this.state.filter}>
-          <ButtonParamete onClick={(event)=>this.setState({choice: 'famille', list:this.state.tri.filter((element)=> element.name === this.state.choice)})}>Famille</ButtonParamete>
-          <ButtonParamete onClick={(event)=>this.setState({choice: 'genre', list:this.state.tri.filter((element)=> element.name === this.state.choice)})}>Genre</ButtonParamete>
-          <ButtonParamete onClick={(event)=>this.setState({choice: 'espece', list:this.state.tri.filter((element)=> element.name === this.state.choice)})}>Espece</ButtonParamete>
-          <ButtonParamete onClick={(event)=>this.setState({choice: 'nom_du_site', list:this.state.tri.filter((element)=> element.name === this.state.choice)})}>Parc</ButtonParamete>
-          </ParameterFiltre>
-        <ListeFiltre >
-            {this.state.list[0] && this.state.list[0].facets.map((item)=>{
-              return(
-              <Li>{item.name}({item.count})</Li>
-              )
-            })}
+          <ButtonParamete
+            valueButton='nom_du_site'
+            onClick={(event) =>
+              this.setState({
+                choice: 'nom_du_site',
+                list: this.state.tri.filter(
+                  (element) => element.name === this.state.choice
+                ),
+              })
+            }
+          >
+            Parc
+          </ButtonParamete>
+          <ButtonParamete
+          valueButton='famille'
+            onClick={(event) =>
+              this.setState({
+                choice: 'famille',
+                list: this.state.tri.filter(
+                  (element) => element.name === this.state.choice
+                ),
+              })
+            }
+          >
+            Famille
+          </ButtonParamete>
+          <ButtonParamete
+            valueButton='genre'
+            onClick={(event) =>
+              this.setState({
+                choice: 'genre',
+                list: this.state.tri.filter(
+                  (element) => element.name === this.state.choice
+                ),
+              })
+            }
+          >
+            Genre
+          </ButtonParamete>
+          <ButtonParamete
+            valueButton='espece'
+            onClick={(event) =>
+              this.setState({
+                choice: 'espece',
+                list: this.state.tri.filter(
+                  (element) => element.name === this.state.choice
+                ),
+              })
+            }
+          >
+            Espece
+          </ButtonParamete>
+        </ParameterFiltre>
+        <ListeFiltre filter={this.state.filter}>
+          <UlListe filter={this.state.filter} filterChoice={this.state.choice}>
+            {this.state.list[0] &&
+              this.state.list[0].facets.map((item) => {
+                return (
+                  <Li>
+                    {item.name}({item.count})
+                  </Li>
+                );
+              })}
+          </UlListe>
         </ListeFiltre>
+        </WindowFilter>
         <Title>Votre collection : 0 / 15</Title>
         <Collection className="collection">
           {this.state.vegetals
-          .filter((element)=> element.fields.photo1)
-          .map((item) => (
-            <DataBase
-              key={item.recordid}
-              famille={item.fields.famille}
-              espece={item.fields.espece}
-              genre={item.fields.genre}
-              image={item.fields.photo1 && item.fields.photo1.id}/>
-          ))}
+            .filter((element) => element.fields.photo1)
+            .map((item) => (
+              <DataBase
+                key={item.recordid}
+                famille={item.fields.famille}
+                espece={item.fields.espece}
+                genre={item.fields.genre}
+                image={item.fields.photo1 && item.fields.photo1.id}
+              />
+            ))}
         </Collection>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Library;
