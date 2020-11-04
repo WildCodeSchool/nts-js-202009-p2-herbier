002b257ed2bc4d2e17709b0fb00c3b1c26e2038c
fdebrabant <debrabant.fanny@gmail.com>
2020-11-04 11:38:15 +0100

    Delet eslint comment

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -83,7 +83,6 @@ const Button = styled.button`
 `;
 
 function Alert(props) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
