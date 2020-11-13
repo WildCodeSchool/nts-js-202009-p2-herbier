5337a8984834325c6b03ef65e360c7c769683a47
Benoit2109 <benoitlebourdonnec@gmail.com>
2020-11-13 12:13:02 +0100

    fixed footer issue

--- src/components/DataBase.jsx
+++ src/components/DataBase.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const Thumbnail = styled.img`
   width: 100px;
   height: 100px;
+  object-fit: cover;
   filter: grayscale(${({ scanned }) => (!scanned ? '100%' : '0%')});
 `;
 
