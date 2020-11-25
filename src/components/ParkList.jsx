aebe2960e5d4314a0f3f96f3d44ed18a1ed03702
fdebrabant <debrabant.fanny@gmail.com>
2020-11-25 12:32:11 +0100

    test

--- src/components/ParkList.jsx
+++ src/components/ParkList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import MapPicker from './logos/map-picker.svg';
 
@@ -40,5 +41,11 @@ function ParkList(props) {
     </List>
   );
 }
+
+ParkList.propTypes = {
+  distance: PropTypes.number.isRequired,
+  namePark: PropTypes.string.isRequired,
+  showNantes: PropTypes.bool.isRequired,
+};
 
 export default ParkList;
